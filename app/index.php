62185701bce205d12dcff6099e20aa24e5ed52a4
lunagradel <lunig@hotmail.com>
2017-05-22 11:59:52 +0200

    added so much

--- app/index.php
+++ app/index.php
@@ -4,8 +4,21 @@
     <meta charset="utf-8">
     <title>eventByte</title>
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/frontpage.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Overpass+Mono" rel="stylesheet">
   </head>
   <body>
+    <div class="gradient frontpage-top">
+      <div class="top">
+        <div class="burger-menu">
+          <div class="menu-line"></div>
+          <div class="menu-line"></div>
+          <div class="menu-line"></div>
+        </div>
+        <?php require_once 'svg/logo.php' ?>
+      </div>
 
+    </div>
   </body>
 </html>
